a034edc45028bf1421be2fec39f2fafb4956a5c4
Dohyungh <bley1217@gmail.com>
2024-02-02 17:57:42 +0900

    0202_ssafy

---
+++ self-study/src/SWEA/_1493_수의새로운연산/Solution.java
@@ -0,0 +1,16 @@
+package SWEA._1493_수의새로운연산;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+
+public class Solution {
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		
+	}
+
+}
