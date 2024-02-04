5bea58541a38c4928814ccfca667278657a59b72
Dohyungh <bley1217@gmail.com>
2024-02-05 00:26:29 +0900

    0205_home

---
+++ self-study/src/SWEA/_1952_수영장/Solution.java
@@ -0,0 +1,34 @@
+package SWEA._1952_수영장;
+
+public class Solution {
+	
+	public static void combination(int[] arr, boolean[] visited, int start, int depth, int r) {
+		if (depth == r) {
+			for(int i =0; i<arr.length; i++) {
+				if(visited[i]) System.out.print(arr[i]+" ");
+			}
+			System.out.println();
+			return;
+		}
+		for (int i = start; i<arr.length; i++) {
+			if (!visited[i]) {
+				visited[i] = true;
+				combination(arr,visited, i+1, depth+1, r);
+				visited[i] = false;
+			}
+		}
+
+	}
+	
+	public static void main(String[] args) {
+		int[] arr = {1,2,3,4};
+		boolean visited[] = new boolean[4];
+		
+		combination(arr, visited, 0,1,2);
+			
+		
+		
+		
+	}
+
+}
