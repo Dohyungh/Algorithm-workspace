10085e080d3a7d1761f844e6f327a6ba99d8ef40
Dohyungh <bley1217@gmail.com>
2024-02-26 01:04:01 +0900

    0226_home

---
+++ self-study/src/BOJ/_15683_감시/Main.java
@@ -0,0 +1,105 @@
+package BOJ._15683_감시;
+
+import java.util.Scanner;
+
+public class Main {
+	static int answer = 0;
+	public static void main(String[] args) {
+		// 최대 4^64(8x8)?
+		// 최대 4^8 = 65536 이래서 최대 cctv 개수를 줬구만?
+		
+		// dfs 너무 복잡해 포기!
+		
+		// 애초에 '연결'관계랄게 없는데 굳이 depth 넣어 가면서 구현할라는게 에바같음.
+		
+		
+		
+//		Scanner sc = new Scanner(System.in);
+//		
+//		int N = sc.nextInt();
+//		int M = sc.nextInt();
+//		
+//		int[][] arr = new int[N][M];
+//		
+//		
+//		int[][][] nodes = new int[8][][];
+//		// 1 이면 column이 4개
+//		// 2 이면 column이 2개
+//		// 3 이면 column이 4개
+//		// 4 이면 column이 4개
+//		// 5 이면 column이 1개
+//		int numNodes = 0;
+//		for (int i = 0; i < N; i++) {
+//			for (int j = 0; j < M; j++) {
+//				int num = sc.nextInt();
+//				arr[i][j] = num;
+//				if (num != 0 && num != 6) {
+//					if (num == 1) {
+//						int[][] node = new int[4][3]; // i, j, num
+//						
+//					}
+//					numNodes++;
+//					
+//				}
+//				
+//			}
+//		}
+//		
+//		//arr 복사를 안하려면
+//		//boolean 배열을 대신 만들고 매번 초기화 해야 할듯?
+//		
+//		boolean[][] monitoring = new boolean[N][M];
+//		boolean[][] visited = new boolean[numNodes][];
+//		// 1 이면 column이 4개
+//		// 2 이면 column이 2개
+//		// 3 이면 column이 4개
+//		// 4 이면 column이 4개
+//		// 5 이면 column이 1개
+//		
+//		
+//		
+//		
+//		sc.close();
+//		
+//	}
+//	
+//	public static int countZero(boolean[][] monitoring) {
+//		int N = monitoring.length;
+//		int M = monitoring[0].length;
+//		
+//		int cnt = 0;
+//		for (int i = 0; i < N; i++) {
+//			for (int j = 0; j < M; j++) {
+//				if (!monitoring[i][j]) cnt++;
+//			}
+//		}
+//		return cnt;
+//	}
+//	
+//	public static void DFS(int[][] nodes, int depth, int r, boolean[][] visited, boolean[][] monitoring) {
+//		if (depth == r+1) {
+//			answer = Math.min(answer, countZero(monitoring));
+//			return;
+//		}
+//		
+//		int type = nodes[depth][2];
+//		for (int i = 0; i < nodes[depth].length; i++) {
+//			if (!visited[depth][i]) {
+//				visited[depth][i] = true;
+//				
+//				monitoring = monitor()
+//				
+//				DFS(nodes,depth+1,r,visited,monitoring);
+//				
+//				visited[depth][i] = false;
+//
+//				
+//			}
+//			
+//			
+//		}
+//		
+	}
+	
+
+}
---
+++ self-study/src/BOJ/_15683_감시/Main_fin.java
@@ -0,0 +1,230 @@
+package BOJ._15683_감시;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main_fin {
+	static int numCases = 1;
+	static int count = 0;
+	static List<int[]> cases = new ArrayList<>();
+	public static void main(String[] args) {
+		// 최대 4^64(8x8)?
+		// 최대 4^8 = 65536 이래서 최대 cctv 개수를 줬구만?
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		
+		int[][] arr = new int[N][M];
+		 
+		// cctv 개수 파악
+		int numcctvs = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				int num = sc.nextInt();
+				arr[i][j] = num;
+				if (num != 0 && num != 6 && num !=5) {
+					numcctvs++;
+				}
+			}
+			
+		}
+		
+		// cctv 배열 생성
+		int[][] cctvs = new int[numcctvs][3]; // List가 싫어서 지난해진 입력과정
+		
+
+		// cctv 종류 파악
+		int cnt = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (arr[i][j] != 0 && arr[i][j] !=6 && arr[i][j] != 5) {
+					cctvs[cnt][0] = arr[i][j];
+					cctvs[cnt][1] = i;
+					cctvs[cnt][2] = j;
+					cnt++;
+					
+				}
+			}
+		}
+		// 예시) [[1,0,0], [1,1,1], [4,1,2]]
+		
+		// 5번 cctv 선반영
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				
+				if (arr[i][j] == 5) {
+					for (int dir = 0; dir < 4; dir++) {
+						arr = fill(arr, i, j, dir);
+					}
+				}
+			}
+		}
+		
+		
+		
+		int[] dirs = new int[numcctvs];
+		for (int i = 0; i < numcctvs; i++) {
+			if (cctvs[i][0] == 1 || cctvs[i][0] == 3 || cctvs[i][0] == 4) {
+				dirs[i] = 4;
+				numCases*=4;
+			}
+			if (cctvs[i][0] == 2) {
+				dirs[i] = 2;
+				numCases*=2;
+			}
+		}
+
+		
+		boolean[][] visited = new boolean[numcctvs][];
+		for(int i = 0; i < numcctvs; i++) {
+			if (cctvs[i][0] == 1 || cctvs[i][0] == 3 || cctvs[i][0] == 4) {
+				visited[i] = new boolean[4]; 
+				
+			} else { // 2일 경우
+				visited[i] = new boolean[2];
+			}
+		}
+		getCases(0, numcctvs, visited);
+
+		int answer = N*M;
+		for (int[] aCase : cases) {
+			
+			//복사 안하면 통과가 안되는데 어디서 얕은 복사 생기는지를 모르겠음ㅠㅠ
+			int[][] temp = new int[N][];
+			for (int i = 0; i < arr.length; i++) {
+				temp[i] = Arrays.copyOf(arr[i], arr[i].length);
+			}
+			temp = getResult(temp, aCase, cctvs);
+			if (answer > countZero(temp)) {
+				answer = Math.min(answer, countZero(temp));
+				for (int l = 0; l < temp.length; l++) {
+
+				}
+
+			}
+			
+		}
+		
+		System.out.println(answer);
+		
+		
+		
+		
+		
+	}
+	// 1 이면 column이 4개
+	// 2 이면 column이 2개
+	// 3 이면 column이 4개
+	// 4 이면 column이 4개
+
+	public static int countZero(int[][] arr) {
+	int N = arr.length;
+	int M = arr[0].length;
+	
+	int cnt = 0;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			if (arr[i][j]==0) cnt++;
+		}
+	}
+	return cnt;
+	}
+	
+	public static int[][] fill (int[][] arr, int i, int j, int dir) {
+		int[] dr = {-1,0,1,0};
+		int[] dc = {0,-1,0,1};
+		int nr = i;
+		int nc = j;
+		int N = arr.length;
+		int M = arr[0].length;
+		while(true) {
+			nr = nr+dr[dir];
+			nc = nc+dc[dir];
+			if(nr < N && nr >= 0 && nc < M && nc>=0) {
+				if (arr[nr][nc] == 6) {
+					break;
+				}
+				if (arr[nr][nc] == 0) arr[nr][nc] = 7; //그냥 아무 숫자
+			}else {
+				break;
+			}
+		}
+		return arr;
+	}
+	
+
+	public static void getCases(int depth, int r, boolean[][] visited) {
+		if (depth == r) {
+			int[] aCase = new int[visited.length];
+			for(int i = 0; i < visited.length; i++) {
+				for (int j = 0; j < visited[i].length; j++) {
+					if (visited[i][j]) aCase[i] = j;
+				}
+			}
+			cases.add(aCase);
+			return;
+		}
+		
+		for (int i = 0; i < visited[depth].length; i++) {
+			if (!visited[depth][i]) {
+				visited[depth][i] = true;
+				getCases(depth+1,r,visited);
+				visited[depth][i] = false;
+			}
+		}
+	
+	}
+	
+	public static int[][] oneFill (int[][] arr, int i, int j, int dir) {
+		arr = fill(arr, i, j, dir);
+		
+		return arr;
+	}
+	public static int[][] twoFill (int[][] arr, int i, int j, int dir) {
+		// 0 0, 2
+		// 1 1, 3
+		arr = fill(arr, i, j, dir);
+		arr = fill(arr, i, j, dir+2);
+		return arr;
+	}
+	public static int[][] threeFill (int[][] arr, int i, int j, int dir) {
+		arr = fill(arr,i,j,dir);
+		arr = fill(arr,i,j,(dir+1)%4);
+		return arr;
+	}
+	public static int[][] fourFill (int[][] arr, int i, int j, int dir) {
+		for(int k = 0; k < 4; k++) {
+			if (k!= dir) {
+				arr = fill(arr,i,j,k);
+			}
+		}
+		return arr;
+	}
+	
+	public static int[][] getResult(int[][] arr, int[] aCase, int[][] cctvs) {
+		int[][] temp = arr;
+		for(int i = 0; i < cctvs.length; i++) {
+			if(cctvs[i][0] ==1) {
+				temp = oneFill(temp, cctvs[i][1], cctvs[i][2], aCase[i]);
+			}
+			if(cctvs[i][0] ==2) {
+				temp = twoFill(temp, cctvs[i][1], cctvs[i][2], aCase[i]);
+			}
+			if(cctvs[i][0] ==3) {
+				temp = threeFill(temp, cctvs[i][1], cctvs[i][2], aCase[i]);
+			}
+			if(cctvs[i][0] ==4) {
+				temp = fourFill(temp, cctvs[i][1], cctvs[i][2], aCase[i]);
+			}
+			
+		}
+		return temp;
+	}
+	
+	
+
+}
