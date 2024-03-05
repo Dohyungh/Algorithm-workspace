6c41303535fda844912f4bfa1187a265bd073e36
Dohyungh <bley1217@gmail.com>
2024-03-05 17:49:43 +0900

    0305_스터디까지

---
+++ self-study/src/BOJ/_16235_나무재테크/Main.java
@@ -0,0 +1,20 @@
+package BOJ._16235_나무재테크;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) {
+		
+//		0.3s
+//		22%
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		// 시뮬
+		
+		
+		
+	}
+
+}
