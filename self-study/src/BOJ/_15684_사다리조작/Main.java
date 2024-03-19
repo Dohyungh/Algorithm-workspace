0e8e12cc2dce70894ff3147815ee80d3c0d4a4e9
Dohyungh <bley1217@gmail.com>
2024-03-19 17:54:15 +0900

    add 사다리조작초안

---
+++ self-study/src/BOJ/_15684_사다리조작/Main.java
@@ -0,0 +1,19 @@
+package BOJ._15684_사다리조작;
+
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		int H = sc.nextInt();
+		
+		for (int m = 0; m < M; m++) {
+			
+		}
+	}
+
+}
