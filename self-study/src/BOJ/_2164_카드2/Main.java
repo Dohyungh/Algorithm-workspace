f0a234d0c5149fbf6aeaa9e1bd1c11b68cf3b46f
Dohyungh <bley1217@gmail.com>
2024-03-20 17:56:10 +0900

    0

---
+++ self-study/src/BOJ/_2164_카드2/Main.java
@@ -0,0 +1,28 @@
+package BOJ._2164_카드2;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		Queue<Integer> queue = new LinkedList<>();
+		
+		for (int i = 1; i < N+1; i++) {
+			queue.add(i);
+		}
+		
+		while(queue.size()!=1) {
+			queue.poll();
+			int n = queue.poll();
+			queue.add(n);
+		}
+		System.out.println(queue.poll());
+	}
+
+}
