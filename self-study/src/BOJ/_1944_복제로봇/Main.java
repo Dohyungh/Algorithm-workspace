33587bf3ed789c58d7a87fba47fe468dcc82b1f4
Dohyungh <bley1217@gmail.com>
2024-04-11 00:21:53 +0900

    0

---
+++ self-study/src/BOJ/_1944_복제로봇/Main.java
@@ -0,0 +1,25 @@
+package BOJ._1944_복제로봇;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		// 머라는 겨
+		
+		// 근데 이건 다익스트라 아닌교?
+		// 250 * 100 * 250 = 625 0000
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		
+		int[][] map = new int[N][N];
+		int[][] copyLocs = new int[M+1][2];
+		
+		
+		
+		
+	}
+
+}
