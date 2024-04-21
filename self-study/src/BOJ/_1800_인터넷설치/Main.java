caebdaed9e01d1beed28e12d5ff60edd63e7cb4a
Dohyungh <bley1217@gmail.com>
2024-04-21 17:52:23 +0900

    0

--- self-study/src/BOJ/_1800_인터넷설치/Main.java
+++ self-study/src/BOJ/_1800_인터넷설치/Main.java
@@ -27,10 +27,21 @@ public class Main {
 		}
 		
 		public void add(Integer num) {
-			if (this.pq.size() <)
+			if (this.pq.size() < K+1) {
+				this.pq.add(num);
+			} else {
+				this.pq.add(num);
+				System.out.println(this.pq.poll());
+			}
+		}
+		@Override
+		public String toString() {
+			return "Node [idx=" + idx + ", cost=" + cost + ", pq=" + pq + "]";
 		}
 		
+		
 	}
+	static int K;
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -38,7 +49,7 @@ public class Main {
 		
 		int N = sc.nextInt();
 		int P = sc.nextInt();
-		int K = sc.nextInt();
+		K = sc.nextInt();
 		
 		List<Node>[] adjList = new ArrayList[N+1];
 		
@@ -53,30 +64,57 @@ public class Main {
 			adjList[to].add(new Node(from, cost));
 		}
 		
-		PriorityQueue<Node> pq = new PriorityQueue<Node>();
+		PriorityQueue<Node> nodePq = new PriorityQueue<Node>();
 		PriorityQueue<Integer> startPq = new PriorityQueue<>();
 		
 		startPq.add(0);
 		
-		Node start = new Node(0, startPq);
+		Node start = new Node(1, startPq);
 		
-		pq.add(start);
+		nodePq.add(start);
 		
 		int[] answer = new int[N+1];
 		Arrays.fill(answer, Integer.MAX_VALUE);
 		
-		while(!pq.isEmpty()) {
-			Node nowNode =pq.poll();
+		while(!nodePq.isEmpty()) {
+			Node nowNode =nodePq.poll();
+			System.out.println(nowNode.pq.peek());
 			
 			if (answer[nowNode.idx] < nowNode.pq.peek()) continue;
 			
-			for (int i = 0; i < adjList[nowNode.idx].size(); i++) {
+			for (Node nextNode : adjList[nowNode.idx]) {
+				System.out.println("nextNode: " + nextNode);
 				
+				
+				PriorityQueue<Integer> tempPq = new PriorityQueue<>();
+				PriorityQueue<Integer> nowPq = nowNode.pq;
+				
+				int size = nowPq.size();
+				
+				for (int i = 0; i < size; i++) {
+					int n = nowPq.poll();
+					tempPq.add(n);
+					nowPq.add(n);
+				}
+				
+				System.out.println(nextNode.cost);
+				Node newNode = new Node(nextNode.idx, tempPq);
+				
+				newNode.add(nextNode.cost);
+				System.out.println(newNode.pq);
+				
+				answer[newNode.idx]= Math.min(answer[newNode.idx], newNode.pq.peek()); 
+				
+				nodePq.add(newNode);
+				
+				System.out.println(newNode);
 			}
+			System.out.println(nodePq);
 			
 			
 			
 		}
+		System.out.println(answer[3]);
 		
 		
 		
