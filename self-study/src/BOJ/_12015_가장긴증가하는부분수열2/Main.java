96fff12b97ff24a9068940dd43c6060d7e33ec5b
Dohyungh <bley1217@gmail.com>
2024-05-02 08:49:42 +0900

    0

--- self-study/src/BOJ/_12015_가장긴증가하는부분수열2/Main.java
+++ self-study/src/BOJ/_12015_가장긴증가하는부분수열2/Main.java
@@ -1,9 +1,33 @@
 package BOJ._12015_가장긴증가하는부분수열2;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		
-		// 시작점을 이분탐색
+		// 숫자와 숫자 사이 간격을 이분탐색
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int[] arr = new int[N];
+		int left = Integer.MAX_VALUE;
+		int right = Integer.MIN_VALUE;
+		for (int i = 0; i < N; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			left = Math.min(left, num);
+			right = Math.max(right, num);
+			arr[i] = num;
+		}
+		
+		int 
+		
 		
 		
 		
