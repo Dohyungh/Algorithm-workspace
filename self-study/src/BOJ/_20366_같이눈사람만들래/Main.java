67609aed349ec266f51d65d739568d10cfdf8c24
Dohyungh <bley1217@gmail.com>
2024-06-20 11:15:34 +0900

    BOJ : 6630-6/20 공통문제 눈사람

--- self-study/src/BOJ/_20366_같이눈사람만들래/Main.java
+++ self-study/src/BOJ/_20366_같이눈사람만들래/Main.java
@@ -1,6 +1,9 @@
 package BOJ._20366_같이눈사람만들래;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -16,25 +19,44 @@ public class Main {
 			arr[i] = sc.nextInt();
 		}
 		
-		int[][] coupleArr = new int[N*N][3];
+		List<int[]> coupleArr = new ArrayList<int[]>();
 		
-		int idx = 0;
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				coupleArr[idx][2] = arr[i] + arr[j];
-				coupleArr[idx][0] = i;
-				coupleArr[idx][1] = j;
-				idx++;
+				if (i < j) {
+					coupleArr.add(new int[] {i, j, arr[i]+arr[j]});
+				}
 			}
 		}
 		
-		Arrays.sort(coupleArr, (o1, o2) -> {return o1[2] - o2[2];});
+		Collections.sort(coupleArr, (o1, o2) -> {return o1[2] - o2[2];});
 		
-		for (int i = 0; i < N*N; i++) {
-			System.out.println(Arrays.toString(coupleArr[i]));			
+//		for (int i = 0; i < coupleArr.size(); i++) {
+//			System.out.println(Arrays.toString(coupleArr.get(i)));			
+//		}
+		
+		int answer = Integer.MAX_VALUE;
+		
+		for (int i = 0 ; i <coupleArr.size(); i++) {
+			int[] a = coupleArr.get(i);
+			
+			int idx1 = a[0];
+			int idx2 = a[1];
+			
+			for (int j = i+1; j < coupleArr.size(); j++) {
+				int[] b = coupleArr.get(j);
+				int idx3 = b[0];
+				int idx4 = b[1];
+				
+				if (idx1 != idx3 && idx1 != idx4 && idx2 != idx3 && idx2 != idx4) {
+					answer = Math.min(answer, Math.abs(a[2] - b[2]));
+					break;
+				}
+			}
+
+			
 		}
-		
-		
+		System.out.println(answer);
 		
 		sc.close();
 		
