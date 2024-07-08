0ebda452a5609edd76b91701d166bb44f07e9cb5
Dohyungh <bley1217@gmail.com>
2024-07-08 17:58:11 +0900

    BOJ : 6630-7/11 개인문제

---
+++ self-study/src/BOJ/_2696_중앙값구하기/Main.java
@@ -0,0 +1,20 @@
+package BOJ._2696_중앙값구하기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int tc = 1; tc <= T; tc++) {
+			int N = Integer.parseInt(br.readLine());
+		}
+		
+		br.close();
+	}
+
+}
