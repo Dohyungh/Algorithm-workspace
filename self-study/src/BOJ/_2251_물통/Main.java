a7f67252f5a7cc5525a3fd0db1867c2685f355d1
Dohyungh <bley1217@gmail.com>
2024-08-06 00:27:04 +0900

    BOJ : 6630-8/8 개인문제 푸는 중

---
+++ self-study/src/BOJ/_2251_물통/Main.java
@@ -0,0 +1,20 @@
+package BOJ._2251_물통;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		
+		List<Integer> arr = new ArrayLi
+		
+		
+	}
+
+}
